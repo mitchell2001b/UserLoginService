5face7404d0ea4adaa31930e505972cce0df65a2
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2024-01-06 21:05:20 +0100

    bug fix for not setting the id manually

--- src/main/java/com/example/UserLoginService/configuration/KeyVaultConfig.java
+++ src/main/java/com/example/UserLoginService/configuration/KeyVaultConfig.java
@@ -1,19 +1,41 @@
 package com.example.UserLoginService.configuration;
 
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeyVaultConfig {
 
-    @Bean
+    /*@Bean
     public SecretClient secretClient() {
         return new SecretClientBuilder()
                 .vaultUrl("https://semeter6kluis.vault.azure.net/")
                 .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+    }*/
+    @Value("${azure.keyvault.client-id}")
+    private String clientId;
+
+    @Value("${azure.keyvault.client-secret}")
+    private String clientSecret;
+
+    @Value("${azure.keyvault.tenant-id}")
+    private String tenantId;
+
+    @Bean
+    public SecretClient secretClient() {
+        return new SecretClientBuilder()
+                .vaultUrl("https://semeter6kluis.vault.azure.net/")
+                .credential(new ClientSecretCredentialBuilder()
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .tenantId(tenantId)
+                        .build())
                 .buildClient();
     }
 }
