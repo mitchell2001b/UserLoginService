169ee3795a34fe0e9f93daf0972a0207a00e85c0
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2024-01-15 15:47:10 +0100

    added refresh tokens actually refreshing

--- src/main/java/com/example/UserLoginService/Account/AccountController.java
+++ src/main/java/com/example/UserLoginService/Account/AccountController.java
@@ -3,6 +3,8 @@ package com.example.UserLoginService.Account;
 import com.example.UserLoginService.AzureServices.KeyVaultService;
 import com.example.UserLoginService.dtos.AccountLoginDto;
 import com.example.UserLoginService.kafka.RegistrationConsumer;
+import com.nimbusds.jose.jwk.JWKException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 import javax.crypto.SecretKey;
 import java.time.Instant;
@@ -75,6 +75,10 @@ public class AccountController
                     .compact();
 
             String refreshToken = Jwts.builder()
+                    .claim("email", dto.getEmail())
+                    .claim("id", dto.getId())
+                    .claim("roleId", dto.getRoleDto().getId())
+                    .claim("roleName", dto.getRoleDto().getName())
                     .setExpiration(Date.from(refreshTokenExpirationDate))
                     .signWith(signingKey)
                     .compact();
@@ -97,5 +101,59 @@ public class AccountController
                     .body(Collections.singletonMap("error", "Wrong email or password"));
         }
 
+    }
+    @PostMapping(value = "/refresh")
+    public ResponseEntity<Map<String, String>> RefreshToken(@RequestBody Map<String, String> refreshTokenData)
+    {
+        String refreshToken = refreshTokenData.get("refresh_token");
+        if (refreshToken != null)
+        {
+            try
+            {
+                secretKey = keyVaultService.getSecretValue("semester6key");
+
+                SecretKey signingKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+                Jws<Claims> jws = Jwts.parser().setSigningKey(signingKey).build().parseClaimsJws(refreshToken);
+                Claims claims = jws.getBody();
+
+                Instant expirationDate = Instant.now().plusSeconds(3600); // Expires 1 hour from now
+
+                String roleName = (String) claims.get("roleName");
+                String email = (String) claims.get("email");
+                Long userId = ((Number) claims.get("id")).longValue();
+                Long roleId = ((Number) claims.get("roleId")).longValue();
+
+                String newAccessToken = Jwts.builder()
+                        .claim("email", email)
+                        .claim("id", userId)
+                        .claim("roleId", roleId)
+                        .claim("roleName", roleName)
+                        .setExpiration(Date.from(expirationDate))
+                        .signWith(signingKey)
+                        .compact();
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", newAccessToken);
+
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .header("Access-Control-Allow-Origin", "*")
+                        .body(tokens);
+
+            }
+            catch (JwtException e)
+            {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .header("Access-Control-Allow-Origin", "*")
+                        .body(Collections.singletonMap("Error", "refresh token invalid"));
+            }
+
+
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .body(Collections.singletonMap("Error", "No valid refresh token provided"));
+        }
     }
 }
